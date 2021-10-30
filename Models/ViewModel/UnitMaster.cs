7dab6d41ec6047ec1652c5d0816e70978b8446df
rht.kumar1 <rht.kumar1@gmail.com>
2021-10-30 13:23:54 +0530

    UnitMaster

---
+++ Models/ViewModel/UnitMaster.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace IMS.Models.ViewModel
+{
+    public class UnitMaster
+    {
+
+        public int UnitId { get; set; }
+        public string Title { get; set; }
+        public string Remarks { get; set; }
+        public bool IsActive { get; set; }
+        public int Createdby { get; set; }
+        public int Loginid { get; set; }
+        public string AppToken { get; set; }
+        public string AuthMode { get; set; }
+        public string ActionMsg { get; set; }
+        public bool IsSucceed { get; set; }
+
+
+        public UnitMaster()
+        {
+            Loginid = CommonUtility.GetLoginID();
+        }
+        public UnitMaster UnitMaster_InsertUpdate()
+        {
+            try
+            {
+                List<SqlParameter> SqlParameters = new List<SqlParameter>();
+                SqlParameters.Add(new SqlParameter("@Unit_Id", UnitId));
+                SqlParameters.Add(new SqlParameter("@Title", Title));
+                SqlParameters.Add(new SqlParameter("@Remarks", Remarks));
+                SqlParameters.Add(new SqlParameter("@Loginid", Loginid));
+                DataTable dt = DBManager.ExecuteDataTableWithParameter("Unit_Master_Insertupdate", CommandType.StoredProcedure, SqlParameters);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    UnitId = Convert.ToInt32(dr[0]);
+                    IsSucceed = Convert.ToBoolean(dr[1]);
+                    ActionMsg = dr[2].ToString();
+                }
+
+            }
+            catch (Exception ex)
+            { throw ex; }
+
+            return this;
+        }
+
+
+        public DataTable UnitMaster_Get()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = DBManager.ExecuteDataTable("Unit_Master_Getdata", CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            { throw ex; }
+
+            return dt;
+        }
+
+        public UnitMaster UnitMaster_Delete()
+        {
+            try
+            {
+                List<SqlParameter> SqlParameters = new List<SqlParameter>();
+                SqlParameters.Add(new SqlParameter("@Unit_Id", UnitId));
+                SqlParameters.Add(new SqlParameter("@Loginid", Loginid));
+                DataTable dt = DBManager.ExecuteDataTableWithParameter("Unit_Master_Delete", CommandType.StoredProcedure, SqlParameters);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    UnitId = Convert.ToInt32(dr[0]);
+                    IsSucceed = Convert.ToBoolean(dr[1]);
+                    ActionMsg = dr[2].ToString();
+                }
+            }
+            catch (Exception ex)
+            { throw ex; }
+
+            return this;
+        }
+
+    }
+
+}
