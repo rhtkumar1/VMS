2ff75287633a0228a7cfc463c0fa296d41b4845d
sandeep <sandeepry92@gmail.com>
2021-12-09 15:35:35 +0530

    commit changes

--- assets/admin/js/custom.js
+++ assets/admin/js/custom.js
@@ -46,9 +46,10 @@ $(function () {
 });
 
 jQuery(window).load(function () { // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(100).css({
+    $('#status').delay(2000).fadeOut; // will first fade out the loading animation
+    $('#status').delay(2000).fadeOut("slow"); // will first fade out the loading animation
+    $('#preloader').delay(2000).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(2000).css({
         'overflow': 'visible'
     });
 })
