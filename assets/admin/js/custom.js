7c4c415ad621b3678c34595f47e139cda054ab63
sandeep <sandeepry92@gmail.com>
2021-12-12 16:12:56 +0530

    action button and watting bar done

--- assets/admin/js/custom.js
+++ assets/admin/js/custom.js
@@ -44,12 +44,3 @@ $(function () {
         }
     });
 });
-
-jQuery(window).load(function () { // makes sure the whole site is loaded
-    $('#status').delay(2000).fadeOut; // will first fade out the loading animation
-    $('#status').delay(2000).fadeOut("slow"); // will first fade out the loading animation
-    $('#preloader').delay(2000).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(2000).css({
-        'overflow': 'visible'
-    });
-})
