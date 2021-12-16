9ac86d74af38fb47b28774d437c83e6eb6d91960
yashwant kumar <yashdlp@gmail.com>
2021-12-16 15:38:18 +0530

    Stationery_Master UI CREATED

--- Models/ViewModel/Stationery_Master.cs
+++ Models/ViewModel/Stationery_Master.cs
@@ -19,19 +19,21 @@ namespace IMS.Models.ViewModel
         public SelectList OfficeLists { get; set; }
         public int Financial_Id { get; set; }
         public SelectList FinancialLists { get; set; }
-        public string  PreFix { get; set; }
+        public string PreFix { get; set; }
         public string Suffix { get; set; }
         public int StartPoint { get; set; }
         public string ActionMsg { get; set; }
         public bool IsSucceed { get; set; }
+        public string AppToken { get; set; }
+        public string AuthMode { get; set; }
 
         public StationeryMaster()
         {
             OfficeLists = new SelectList(DDLValueFromDB.GETDATAFROMDB("office_Id", "Title", "Office_Master", "And IsActive=1"), "Id", "Value");
             MenuLists = new SelectList(DDLValueFromDB.GETDATAFROMDB("Menu_Id", "Menu_Name", "Menu_Master", "And IsActive = 1 AND IsStationery=1"), "Id", "Value");
-            string FYWhereClouse = "And M.IsActive = 1 and M.Company_Id= " +  CommonUtility.GetCompanyID().ToString();
+            string FYWhereClouse = "And M.IsActive = 1 and M.Company_Id= " + CommonUtility.GetCompanyID().ToString();
             FinancialLists = new SelectList(DDLValueFromDB.GETDATAFROMDB("M.Financial_Id", "C.Title + ' ('+ Cast(YEAR(M.From_date) AS nvarchar)+'-'+Cast(YEAR(M.To_date) AS nvarchar) +')' ", "Financial_Master AS M inner join Company_Master AS C ON M.Company_Id=C.Company_Id", FYWhereClouse), "Id", "Value");
-            
+
         }
 
         public StationeryMaster Stationery_Master_InsertUpdate()
@@ -44,11 +46,12 @@ namespace IMS.Models.ViewModel
                 SqlParameters.Add(new SqlParameter("@Company_Id", CommonUtility.GetCompanyID()));
                 SqlParameters.Add(new SqlParameter("@Office_Id", Office_Id));
                 SqlParameters.Add(new SqlParameter("@Menu_Id", Menu_Id));
+                SqlParameters.Add(new SqlParameter("@Financial_Id", Financial_Id));
                 SqlParameters.Add(new SqlParameter("@PreFix", PreFix));
                 SqlParameters.Add(new SqlParameter("@Suffix", Suffix));
                 SqlParameters.Add(new SqlParameter("@StartPoint", StartPoint));
                 SqlParameters.Add(new SqlParameter("@CreatedBy", CommonUtility.GetLoginID()));
-                DataTable dt = DBManager.ExecuteDataTableWithParameter("Financial_Master_Insertupdate", CommandType.StoredProcedure, SqlParameters);
+                DataTable dt = DBManager.ExecuteDataTableWithParameter("Stationery_Master_Insertupdate", CommandType.StoredProcedure, SqlParameters);
                 foreach (DataRow dr in dt.Rows)
                 {
                     Stationery_ID = Convert.ToInt32(dr[0]);
@@ -62,5 +65,27 @@ namespace IMS.Models.ViewModel
             return this;
         }
 
+        internal DataTable StationeryMaster_Get()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = DBManager.ExecuteDataTable("Stationery_GetData", CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            { throw ex; }
+
+            return dt;
+        }
+
+        
+        internal bool StationeryMaster_Delete(int StationeryId)
+        {
+            List<SqlParameter> SqlParameters = new List<SqlParameter>();
+            SqlParameters.Add(new SqlParameter("@Stationery_ID", StationeryId));
+            SqlParameters.Add(new SqlParameter("@ModifiedBy", CommonUtility.GetLoginID()));
+            DataTable dt = DBManager.ExecuteDataTableWithParameter("Stationery_Master_Delete", CommandType.StoredProcedure, SqlParameters);
+            return Convert.ToBoolean(dt.Rows[0]["IsSucceed"]);
+        }
     }
 }
