69983366dfdf7132470bd459ad32953014f131d8
rht.kumar1 <rht.kumar1@gmail.com>
2022-02-03 18:17:56 +0530

    report changes : rohit

---
+++ Reports/ReportViewer.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+
+
+namespace IMS.Reports
+{
+    public partial class ReportViewer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string SaleId = "20";
+            ReportViewer1.ProcessingMode = ProcessingMode.Local;
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath("/Reports/Report/Material_Sale_Bill.rdlc");
+            SqlCommand cmdLatest = OpenConnection("Report_Material_Sale_Bill");
+            cmdLatest.Parameters.Add(new SqlParameter("@SaleId", SaleId));
+            DataSet dt = GetDataSet(cmdLatest);
+            ReportViewer1.LocalReport.DataSources.Clear();
+            ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DT_Material_Sale_Bill", dt.Tables[0]));
+        }
+
+        public SqlCommand OpenConnection(string strSpName)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = strSpName;
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            return cmd;
+        }
+        public DataSet GetDataSet(SqlCommand cmd)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
