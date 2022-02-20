cfe0316f3a897be6592443a95564463d4d4054ce
rht.kumar1 <rht.kumar1@gmail.com>
2022-02-20 19:38:58 +0530

    date format changes : rohit

--- assets/common.js
+++ assets/common.js
@@ -227,7 +227,7 @@ $(function () {
     var date_input = $('.clsDate'); //our date input has the name "date"
     var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
     var options = {
-        dateFormat: 'dd-mm-yy',
+        dateFormat: 'dd/mm/yy',
         container: container,
         todayHighlight: true,
         autoclose: true,
