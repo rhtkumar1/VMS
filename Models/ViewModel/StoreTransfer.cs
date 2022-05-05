525b2d6fc1ca98bb0c57ed2cb95a6a2db4b666c2
Yashwant kumar <yashdlp@gmail.com>
2022-05-05 10:34:00 +0530

    Store transfar

--- Models/ViewModel/StoreTransfer.cs
+++ Models/ViewModel/StoreTransfer.cs
@@ -1,6 +1,8 @@
 ﻿using IMS.Models.CommonModel;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,6 +11,7 @@ namespace IMS.Models.ViewModel
 {
     public class StoreTransfer
     {
+        public int TransfarID { get; set; }
         public int ToOffice_Id { get; set; }
         public SelectList ToOfficeLists { get; set; }
         public int FromOffice_Id { get; set; }
@@ -16,6 +19,8 @@ namespace IMS.Models.ViewModel
         public List<StoreTransferList> StoreTransferList { get; set; }
         public string Date { get; set; }
         public string Remarks { get; set; }
+        public string StoreData { get; set; }
+        public string RefrenceNumber { get; set; }
 
         public SelectList Item_Lists { get; set; }
         public SelectList UnitLists { get; set; }
@@ -47,6 +52,48 @@ namespace IMS.Models.ViewModel
             StoreTransferList = new List<StoreTransferList>();
             
         }
+
+        //StoreTransfar_InsertUpdate
+
+        //public StoreTransfer InsertUpdate()
+        //{
+        //    try
+        //    {
+        //        var sb = new System.Text.StringBuilder();
+        //        foreach (var item in StoreTransferList)
+        //        {
+        //            sb.AppendLine(@"<listnode Line_Id=""" + Convert.ToString(item.Line_Id) + @"""  SaleLine_Id=""" + Convert.ToString(item.SaleLine_Id) + @""" 
+        //                           Item_Id=""" + Convert.ToString(item.Item_Id) + @"""  Quantity=""" + Convert.ToString(item.Quantity) + @"""
+        //                           Unit_Id=""" + Convert.ToString(item.Sale_Unit) + @"""  GatePass_Office_Id=""" + Convert.ToString(item.GatePass_Office_Id) + @""" />");
+        //        }
+        //        StoreData = "<Line>" + sb + "</Line>";
+        //        // GatePass_Id = 0;
+        //        List<SqlParameter> SqlParameters = new List<SqlParameter>();
+        //        SqlParameters.Add(new SqlParameter("@TransfarID", TransfarID));
+        //        SqlParameters.Add(new SqlParameter("@RefrenceNumber", RefrenceNumber));
+        //        SqlParameters.Add(new SqlParameter("@FromOffice_ID", FromOffice_Id));
+        //        SqlParameters.Add(new SqlParameter("@ToOffice_ID", ToOffice_Id));
+        //        SqlParameters.Add(new SqlParameter("@TransationDate", Loginid));
+        //        SqlParameters.Add(new SqlParameter("@Remarks", Remarks));
+
+
+
+
+        //        SqlParameters.Add(new SqlParameter("@USERID", Loginid));
+        //        SqlParameters.Add(new SqlParameter("@Material_Line", GatePass_Id));
+        //        DataTable dt = DBManager.ExecuteDataTableWithParameter("StoreTransfar_InsertUpdate", CommandType.StoredProcedure, SqlParameters);
+        //        foreach (DataRow dr in dt.Rows)
+        //        {
+        //            TransfarID = Convert.ToInt32(dr[0]);
+        //            IsSucceed = Convert.ToBoolean(dr[1]);
+        //            ActionMsg = dr[2].ToString();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    { throw ex; }
+
+        //    return this;
+        //}
 
     }
 
